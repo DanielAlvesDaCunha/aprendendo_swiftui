ac5218c5226cf11ae425499ec6491566fa942781
Daniel Alves <Danielalvestrab@gmail.com>
2024-11-11 13:19:55 -0300

    Initial Commit

---
+++ aprendendo_swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  aprendendo_swiftui
+//
+//  Created by Fiocruz on 11/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ aprendendo_swiftui/aprendendo_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  aprendendo_swiftuiApp.swift
+//  aprendendo_swiftui
+//
+//  Created by Fiocruz on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct aprendendo_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
